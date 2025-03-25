459afc98b4e88adcaf854c2f58c431792f10fc5c
Matias Vazquez <134975128+MatiasEzequielVazquez@users.noreply.github.com>
2025-03-25 19:17:41 +0100

    Update MI-APELLIDO_TP-01.py
    Adding resolution of TP-01

---
+++ 01 Estructuras Secuenciales/MI-APELLIDO_TP-01.py
@@ -0,0 +1,41 @@
+#1)
+""" print("Hola Mundo!") """
+#2)
+""" nombre = input("Cual es tu nombre?")
+print(f"Hola {nombre}!") """
+#3)
+""" nombre = input("Ingresa tu nombre:")
+apellido = input("Ingresa tu apellido:")
+edad = input("Ingresa tu edad:")
+residencia = input("Ingresa tu lugar de residencia:")
+print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}.") """
+#4)
+""" import math
+radio = int(input("Ingresa el radio de un circulo:"))
+area = math.pi * math.pow(radio, 2)
+perimetro = 2 * math.pi * radio
+print(f"Para un circulo de radio: {radio} el area es: {area} y el perimetro es: {[perimetro]}") """
+#5)
+""" segundos = int(input("Ingrese una cantidad de segundos:"))
+print(f"La cantidad de segundos {segundos} equivale a {segundos/3600} horas") """
+#6)
+""" num = int(input("Ingresa un numero:"))
+print(f"La tabla de multiplicar para {num} es:")
+for i in range (1, 11):
+    print(f"{num} * {i} = {num * i}") """
+#7)
+""" num1 = int(input("Ingresa un numero:"))
+num2 = int(input("Ingresa otro numero:"))
+print(f"Suma: {num1 + num2} \nResta: {num1 - num2} \nMultiplicacion: {num1 * num2} \nDivision: {num1 / num2}") """
+#8)
+""" peso = float(input("Cual es tu peso en kg? "))
+altura = float(input("Cual es tu altura en M? "))
+print(f"Tu indice de masa corporal (IMC) es: {peso/(altura**2)}") """
+#9)
+""" celcius = float(input("Cual es la temperatura en grados Celcius? "))
+print(f"El equivalente de {celcius} grados Celcius en Fhrenheit es de: {9/5 * celcius +32}") """
+#10)
+""" num1 = float(input("Ingresa un numero: "))
+num2 = float(input("Ingresa otro numero: "))
+num3 = float(input("Ingresa un tercer numero: "))
+print(f"El promedio de los numeros ingresados es: {(num1 + num2 + num3)/3}") """
